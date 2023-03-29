caaa68c706c284997b1c18781bf8e300db520fba
tuantran221 <trieutuan22012001@gmail.com>
2023-03-29 10:20:06 +0700

    set up docker compose to connect postgres database

---
+++ src/main/java/com/singleapi/Customer.java
@@ -0,0 +1,70 @@
+package com.singleapi;
+
+import java.util.Objects;
+
+public class Customer {
+    private int id;
+    private String name;
+    private String email;
+    private String age;
+
+    public Customer(
+            int id,
+            String name,
+            String email,
+            String age)
+    {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Customer(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Customer customer)) return false;
+        return id == customer.id && Objects.equals(name, customer.name) && Objects.equals(email, customer.email) && Objects.equals(age, customer.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, age);
+    }
+}
